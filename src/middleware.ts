8fc92fa1aa65c797ad78a49a56e618c9f70f7261
Paul Mathy <paul.mathy@ecam.fr>
2026-03-24 21:22:32 +0100

    Fix middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,19 +2,19 @@ import { createServerClient } from '@supabase/ssr'
 import { NextResponse, type NextRequest } from 'next/server'
 
 export async function middleware(request: NextRequest) {
-  let supabaseResponse = NextResponse.next({ request })
+  let response = NextResponse.next({ request })
 
   const supabase = createServerClient(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
     process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
     {
       cookies: {
-        getAll() { return request.cookies.getAll() },
+        getAll() {
+          return request.cookies.getAll()
+        },
         setAll(cookiesToSet) {
-          cookiesToSet.forEach(({ name, value }) => request.cookies.set(name, value))
-          supabaseResponse = NextResponse.next({ request })
           cookiesToSet.forEach(({ name, value, options }) =>
-            supabaseResponse.cookies.set(name, value, options)
+            response.cookies.set(name, value, options)
           )
         },
       },
@@ -22,7 +22,7 @@ export async function middleware(request: NextRequest) {
   )
 
   await supabase.auth.getUser()
-  return supabaseResponse
+  return response
 }
 
 export const config = {
